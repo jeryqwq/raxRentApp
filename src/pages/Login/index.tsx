b90b8b7680faa120ae584064ada64083405b117b
chenjie <jery1997@foxmail.com>
2022-08-24 23:20:53 +0800

    feat: 登录对接获取openid, todo： 注册

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,26 +2,50 @@ import { createElement } from 'rax';
 import styles from './index.module.less';
 import Image from 'rax-image';
 import { isWeChatMiniProgram } from '@uni/env';
+import { myRequest } from '@/utils';
+import { setStorage } from '@uni/storage';
 
 function Login() {
   return (
     <div>
       <div className={styles['login-wrap']}>
         <Image style={{width: '600rpx', height: '300rpx'}} source={{ uri: 'http://121.204.145.151:8087/icons/head2.png'}}/>
-        <div onClick={() => {
-          if(isWeChatMiniProgram) {
+        <div
+          onClick={() => {
+          if (isWeChatMiniProgram) {
             wx.login({
-              success (res) {
+              success: async (res) =>  {
                 if (res.code) {
-                  //发起网络请求
-                 console.log(res.code)
+                  const res2 = await myRequest({
+                    data: { code: res.code },
+                    mehtod: 'get',
+                    url: 'login/getOpenId',
+                  })
+                    const { openid } = res2;
+                    setStorage({
+                      key: 'openid',
+                      data: openid,
+                    });
+                  const res3 = await myRequest({
+                    data: {openid},
+                    url: "/login/openIdLogin",
+                    method: 'get'
+                  })
+                  if(res3) {
+                    console.log(res3)
+                    // setStorage({
+                    //   key: 'key',
+                    //   data: res3,
+                    // });
+                  }
                 } else {
                   console.log('登录失败！' + res.errMsg)
                 }
-              }
+              },
             })
           }
-        }} className={'login'} style={{marginTop: '70px'}}>
+        }} 
+        className={'login'} style={{marginTop: '70px'}}>
           微信用户一键登录
         </div>
         <div className={'login reg'}>
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,23 @@
-import { isWeChatMiniProgram } from '@uni/env';
+import request from '@uni/request';
+import { showToast } from '@uni/toast';
 
-export const request =  ({ url, data, params, method }) =>  {
-  if(isWeChatMiniProgram) {
-    return wx.request({
-      url,
-      data,
-      params,
-      method
+export const myRequest = (params) => {
+  return new Promise((resolve, rej) => {
+    request({
+      ...params,
+      url: 'https://www.fjrongshengda.com/lease-center/' + params.url,
+      success: (res) => {
+        if (res.data.code === '0') {
+          resolve(res.data.data)
+        } else {
+          showToast({
+            content: res.data.msg,
+            type: 'fail',
+            duration: 1000,
+          });
+          rej(res.data.msg)
+        }
+      },
     })
-  }
+  })
 }
